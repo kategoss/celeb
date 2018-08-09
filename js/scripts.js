e4d1694344cd9741a8cee3265fa518666197dae7
Kate Goss and Liza Tennis <kate@>
2018-08-09 08:57:29 -0700

    add if else function to js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
   var age = parseInt(prompt("How old are you?"));
 
-  if(age >= 18) {
+  if(age > 18) {
+    $("#of_age").show();
+  } else if (age === 18) {
+    alert("Congratulations! Have a great first vote!");
     $("#of_age").show();
   } else {
     $("#not_of_age").show();
